63368da0f48f2e31dd13676aca66923d4fbc358c
guilherme.092011 <guilherme.092011@hotmail.com>
2021-02-07 14:53:16 -0300

    learning react

---
+++ react/my-app/src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
+import notes from '../notes';
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      {
+        notes.map(note => {
+          return (
+            <Note
+              key={note.key}
+              title={note.title}
+              content={note.content}
+            />
+          );
+        })
+      }
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ react/my-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; {new Date().getFullYear()}</p>
+        </footer>
+    );
+};
+
+export default Footer;
